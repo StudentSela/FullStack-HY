fa59a772a3731327aece2e3ca9c8363e1792bd68
Elias Tupasela <etupasel22@student.oulu.fi>
2024-02-18 13:16:20 +0200

    Exercises1.1-1.5

--- exercises1.1-1.5/App.jsx
+++ exercises1.1-1.5/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
 
-function App() {
-  const [count, setCount] = useState(0)
+  const Header = ({ courseName }) => <h1>{courseName}</h1>;
+
+  const Content = ({ parts }) => (
+    <div>
+      <Part partName={parts[0].name} exercises={parts[0].exercises} />
+      <Part partName={parts[1].name} exercises={parts[1].exercises} />
+      <Part partName={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  );
+
+  const Total = ({ parts }) => (
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  );
+
+  const Part = ({ partName, exercises }) => <p>{partName} {exercises}</p>;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+export default App;
